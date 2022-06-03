5eebc50e5ba047a3fd360f73fa7d27486f87ace8
qutieee <100053564+qutieee@users.noreply.github.com>
2022-06-04 04:39:13 +0800

    Update connection.php

--- dataCenter/connection.php
+++ dataCenter/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('sql200.epizy.com','epiz_31880529','SbwHbdC0JnK
-','epiz_31880529_aces_db');
+$conn = mysqli_connect('remotemysql.com','dvF6QpJzV5','1m9nfUVa1e
+','dvF6QpJzV5');
 
 ?>
